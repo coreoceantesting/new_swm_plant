ee1bd0ed6a752ad148c6ac12cf701e91dd6ee560
coreoceantesting <coreoceantesting@gmail.com>
2024-03-28 14:38:29 +0530

    create api for weight Machine

---
+++ app/Models/WeightMachine.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WeightMachine extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'trip_id', 'ledger_id', 'item_id', 'ticket_no', 'weightype', 'vehicle_no', 'party_code', 'item_code', 'charges', 'dc_no', 'grosswt', 'tarewt', 'netwt', 'gross_date', 'taredate', 'netdate', 'paymentmode', 'party_name', 'product_name'
+    ];
+}
