c7b311d3fb9b64ea8721d4e151939d852975cf3a
coreoceantesting <coreoceantesting@gmail.com>
2024-03-28 11:57:41 +0530

    git connection ok

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,5 +1,5 @@
 <x-admin.layout>
-    <x-slot name="title">Test</x-slot>
+    <x-slot name="title">Dashboard</x-slot>
     <x-slot name="heading">Dashboard</x-slot>
     {{-- <x-slot name="subheading">Test</x-slot> --}}
 
