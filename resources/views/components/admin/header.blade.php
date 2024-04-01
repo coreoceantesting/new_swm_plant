d5b3837e9fee014a51a5d956ede3f3d5f7d156e0
coreoceantesting <coreoceantesting@gmail.com>
2024-04-01 17:42:46 +0530

    hide profile form header

--- resources/views/components/admin/header.blade.php
+++ resources/views/components/admin/header.blade.php
@@ -183,7 +183,7 @@
                         <h6 class="dropdown-header">
                             Welcome {{ ucfirst(auth()->user()->name) }}!
                         </h6>
-                        <a class="dropdown-item" href="pages-profile.html"><i class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i>
+                        <a class="dropdown-item d-none" href="pages-profile.html"><i class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i>
                             <span class="align-middle">Profile</span>
                         </a>
                         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
