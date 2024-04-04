07f85bb639eea3b57b807bf4fc5f18f06a81aa7c
coreoceantesting <coreoceantesting@gmail.com>
2024-04-04 17:01:46 +0530

    add new fields

--- app/Models/WeightMachine.php
+++ app/Models/WeightMachine.php
@@ -9,6 +9,6 @@ class WeightMachine extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'user_id','tripID','Ticket_No', 'WeighType', 'Vehicle_No', 'Party_Code', 'Item_Code', 'Charges', 'GrossWt', 'TareWt', 'NetWt', 'GrossDate', 'GrossTime','TareDate', 'TareTime','EntryType', 'PaymentMode', 'Party_Name', 'Product_Name','Field1','Field2','Field3','File_Path_1','File_Path_2','File_Path_3','File_Path_4','File_Path_6','File_Path_7','File_Path_8','Multi_Weigh_Group_No','AutoManual','Weight_Seq','Pcs','Field4','Field5','Field6','Stable_Gross_Tare','Reference_Ticket_No','Wb_Id','Is_Offline_Upload','Net_Date','CoCode','YearID','UserID','EntryDate','UpdateDate'
+        'user_id','tripID','Ticket_No', 'WeighType', 'Vehicle_No', 'Party_Code', 'Item_Code', 'Charges', 'GrossWt', 'TareWt', 'NetWt', 'GrossDate', 'GrossTime','TareDate', 'TareTime','EntryType', 'PaymentMode', 'Party_Name', 'Product_Name','Field1','Field2','Field3','File_Path_1','File_Path_2','File_Path_3','File_Path_4','File_Path_6','File_Path_7','File_Path_8','Multi_Weigh_Group_No','AutoManual','Weight_Seq','Pcs','Field4','Field5','Field6','Stable_Gross_Tare','Reference_Ticket_No','Wb_Id','Is_Offline_Upload','Net_Date','CoCode','YearID','UserID','EntryDate','UpdateDate', 'modified_date', 'Img1', 'Img2', 'Img3', 'Img4', 'Img5', 'Img6', 'Img7', 'Img8'
     ];
 }
