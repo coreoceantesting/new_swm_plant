d862de8567c192f161723da1c3e43d4b9f7de070
coreoceantesting <coreoceantesting@gmail.com>
2024-05-23 18:52:19 +0530

    remove change

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Barryvdh\DomPDF\Facade as PDF;
+use PDF;
 use App\Models\WeightMachine;
 
 class ExportController extends Controller
